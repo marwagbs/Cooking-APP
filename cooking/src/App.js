2e852243d58d05219cf977b6b53b666492105cbe
marwagbs <90385255+marwagbs@users.noreply.github.com>
2023-10-12 09:54:02 +0200

    Update App.js

--- cooking/src/App.js
+++ cooking/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import MediaCard from "./Components/Card";
+
+
 
 function App() {
+  const [data, useData]=React.useState([]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MediaCard />
     </div>
   );
 }
