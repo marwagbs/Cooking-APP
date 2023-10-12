976bde24c4429f9e712986d6a61293d993a1b90c
marwagbs <90385255+marwagbs@users.noreply.github.com>
2023-10-12 12:16:22 +0200

    Update App.js

--- cooking/src/App.js
+++ cooking/src/App.js
@@ -1,15 +1,57 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import MediaCard from "./Components/Card";
-
-
+import axios from "axios";
+import "./index.css"
 
 function App() {
-  const [data, useData]=React.useState([]);
-  
+  const [data, setData] = useState([]);
+  const [inputSearch, setInputSearch]=useState("");
+ 
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputSearch}`
+      )
+      .then((data) => {
+        console.log(data.data);
+        setData(data.data.meals);
+      });
+  }, [inputSearch]);
+
   return (
-    <div className="App">
-      <MediaCard />
-    </div>
+    <>
+      <h1>Cooking APP</h1>
+      <div>
+        <input
+          type="text"
+          id="search"
+          name="search"
+          placeholder="Type the name of a food"
+          onChange={(e)=>setInputSearch(e.target.value)}
+        ></input>
+      </div>
+
+      <div className="card">
+        
+        {
+        data &&
+        data.map((item) => {
+          return (
+            <div key={item.idMeal}>
+              <MediaCard
+                title={item.strMeal}
+                origin={item.strArea}
+                src={item.strMealThumb}
+                description={item.strInstructions}
+                alt={item.strMeal}
+                source={item.strSource}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
